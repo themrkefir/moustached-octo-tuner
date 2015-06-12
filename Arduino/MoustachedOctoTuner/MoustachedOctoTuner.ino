7d14a6a09cb5fd6e45a5f65f76be68ca452ee1b6
Kefir <themrkefir@gmail.com>
2015-06-12 19:10:50 +0300

    Calculation music notes and its output to the serial port.

--- Arduino/MoustachedOctoTuner/MoustachedOctoTuner.ino
+++ Arduino/MoustachedOctoTuner/MoustachedOctoTuner.ino
@@ -1,3 +1,5 @@
+#include <math.h>
+
 const int n PROGMEM = 256;
 
 const byte rsin[256] PROGMEM = {
@@ -24,7 +26,40 @@ const byte rsin[256] PROGMEM = {
  29,    31,    33,    35,    37,    40,    42,    44,    47,    49,    52,
  54,    57,    59,    62,    65,    67,    70,    73,    76,    79,    82,
  85,    88,    90,    93,    97,    100,   103,   106,   109,   112,   115,
- 118,   121,   124};
+ 118,   121,   124}; 
+
+const char half_tones[] PROGMEM  = {
+"C2 \0C#2\0D2 \0D#2\0E2 \0F2 \0F#2\0G2 \0G#2\0A2 \0A#2\0H2 \0C3 \0C#3\0D3 \0D#3\0E3 \0F3 \0F#3\0G3 \0G#3\0A3 \0A#3\0H3 \0C4 \0C#4\0D4 \0D#4\0E4 \0F4 \0F#4\0G4 \0G#4\0A4 \0A#4\0H4 \0C5 \0C#5\0D5 \0D#5\0E5 \0"};
+
+const byte notes[12] PROGMEM = 
+{
+  B00000000, //C
+  B00000001, //C#
+  B00000000, //D
+  B00000001, //D#
+  B00000000, //E 
+  B00000000, //F
+  B00000001, //F#
+  B00000000, //G
+  B00000001, //G#
+  B00000000, //A
+  B00000001, //A#
+  B00000000  //B
+};
+
+const byte numbers[10] PROGMEM =
+{
+  B00000000, //0
+  B00000000, //1
+  B00000000, //2
+  B00000000, //3
+  B00000000, //4
+  B00000000, //5
+  B00000000, //6
+  B00000000, //7
+  B11111110, //8
+  B00000000  //9
+};
 
 int sins[254];
 int coss[254];
@@ -128,8 +163,6 @@ void loop()
   {
     int p = abs(sins[i] / 16) + abs(coss[i] / 16);
     
-    //Serial.println(p);
-    
     if(p > p_max)
     {
       i_max = i;
@@ -137,10 +170,37 @@ void loop()
     }
   }
   
+  float k = (i_max + 1) * 0.06; // ~ * 3.9 / 65.406;
+  
+  float half_tone = log10(k) * 39.863; // ~ * 12 / log10(2);
+  
+  int half_tone_index = -1;
+  
+  if(half_tone > 0 && half_tone < 39.4)
+    half_tone_index = (int)(round(half_tone));
+  
   Serial.println();
   Serial.print(p_max);
   Serial.print(F(" f = "));
   Serial.println((i_max + 1) * 39);
+  Serial.println();
+  Serial.print(F(" n = "));
+  if(half_tone_index == -1)
+     Serial.println(F("So low..."));
+  else
+  {
+    i = half_tone_index * 4;
+    while(true)
+    {
+      temp = pgm_read_byte_near(half_tones + i);
+      if(temp != '\0')
+        Serial.print((char)temp);
+      else
+        break;
+        
+      i++;
+    }
+  }
   Serial.println();
   
   /*Serial.println();
